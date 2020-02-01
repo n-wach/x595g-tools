c00f35976cc4e1d1e940129d9a4fa146afbbfa34
strongwar <56051366+strongwar@users.noreply.github.com>
2020-01-31 19:37:19 -0800

    update

--- by7efile/by7e.py
+++ by7efile/by7e.py
@@ -2,10 +2,9 @@ from bitarray import bitarray
 
 b = bitarray(endian='big')
 b.fromfile(open("flag.txt","rb"))
-len = b.length()
 arr = []
-len = len // 7
-place = 0
+len = b.length() // 7
+
 for i in range(len):
    arr.append(b[i*7:i*7 +7])
    arr[i].reverse()
