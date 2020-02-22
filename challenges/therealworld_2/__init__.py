5a90af200e23db82b46c4d77a5382e49f348956f
n-wach <nmwachholz@gmail.com>
2020-02-21 18:29:01 -0800

    Added real_world_2, disassembler is actually still broken :(

---
+++ challenges/therealworld_2/__init__.py
@@ -0,0 +1,16 @@
+import by7efile
+import disassembler
+from readorc import orc
+
+o = orc.Orc("chall2.exe")
+
+info = o.to_dict()
+
+text = info["segment_table"]["segments"][1]
+
+exe = o.o.contents[text["offset"]:text["offset"] + text["size"]]
+
+disassembler.disassemble(exe)
+
+# read disassembly for the flag
+
